2568bf67d65011a5bfd1f91cf370277670ee26d0
Alex Godzilla <aadev151@vk.com>
2022-10-26 21:16:29 +0200

    Fixed subtests: ready for review

--- homework/catalog/tests.py
+++ homework/catalog/tests.py
@@ -12,61 +12,32 @@ class StaticURLTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_catalog_items_endpoint(self):
-
-        tests = tuple(
-            # Testing int items
-            [
-                {
-                    'primary_key': pk,
-                    'necessary_status': 200,
-                    'description': f'Testing /catalog/{pk}/'
-                }
-                for pk in range(1, 200)
-            ] +
-
-            # Testing float items
-            [
-                {
-                    'primary_key': pk,
-                    'necessary_status': 404,
-                    'description': f'Testing /catalog/{pk}/'
-                }
-                for pk in [.05 * i for i in range(1, 41)]
-            ] +
-
-            # Testing zero and negative int items
-            [
-                {
-                    'primary_key': pk,
-                    'necessary_status': 404,
-                    'description': f'Testing /catalog/{pk}/'
-                }
-                for pk in range(-20, 1)
-            ] +
-
-            # Testing strings and mixed items
-            [
-                {
-                    'primary_key': 'azino',
-                    'necessary_status': 404,
-                    'description': 'Testing /catalog/azino/'
-                },
-                {
-                    'primary_key': 'hey123',
-                    'necessary_status': 404,
-                    'description': 'Testing /catalog/hey123/'
-                },
-                {
-                    'primary_key': 'p12i',
-                    'necessary_status': 404,
-                    'description': 'Testing /catalog/p12i/'
-                },
-                {
-                    'primary_key': '1234567u',
-                    'necessary_status': 404,
-                    'description': 'Testing /catalog/1234567u/'
-                }
-            ]
+        tests = (
+            {
+                'primary_key': 123,
+                'necessary_status': 200,
+                'description': 'Testing positive int'
+            },
+            {
+                'primary_key': 15.73,
+                'necessary_status': 404,
+                'description': 'Testing float'
+            },
+            {
+                'primary_key': -15,
+                'necessary_status': 404,
+                'description': 'Testing negative int'
+            },
+            {
+                'primary_key': 'azino',
+                'necessary_status': 404,
+                'description': 'Testing string'
+            },
+            {
+                'primary_key': '1234567u',
+                'necessary_status': 404,
+                'description': 'Testing mixed string'
+            }
         )
 
         for test in tests:
