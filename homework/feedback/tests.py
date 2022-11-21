8ae0b0db216013b808b82d5bf3b8be6f4dca7575
Alex Godzilla <aadev151@vk.com>
2022-11-21 21:10:10 +0100

    Lesson 6: Ready for review: Fix issue #10

--- homework/feedback/tests.py
+++ homework/feedback/tests.py
@@ -1,19 +1,23 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from feedback.forms import FeedbackForm
 from feedback.models import Feedback
 
 
 class FeedbackFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.form = FeedbackForm()
+
     def test_form_is_in_context(self):
         response = Client().get(reverse('feedback:feedback'))
         self.assertIn('form', response.context)
 
     def test_correct_labels_and_help_texts(self):
-        response = Client().get(reverse('feedback:feedback'))
-        form = response.context['form']
-        text_field = form.fields[Feedback.text.field.name]
-        self.assertEquals(text_field.label, 'Текст отзыва')       
+        text_field = self.form.fields[Feedback.text.field.name]
+        self.assertEquals(text_field.label, 'Текст отзыва')
         self.assertEquals(text_field.help_text, 'Оставьте здесь свой отзыв')
 
     def test_redirect_and_creating_database_record_after_submitting_form(self):
@@ -30,3 +34,8 @@ class FeedbackFormTests(TestCase):
 
         self.assertRedirects(response, reverse('feedback:feedback'))
         self.assertEquals(Feedback.objects.count(), feedback_count + 1)
+        self.assertTrue(
+            Feedback.objects.filter(
+                text=form_data['text']
+            ).exists()
+        )
--- homework/feedback/views.py
+++ homework/feedback/views.py
@@ -1,3 +1,4 @@
+from decouple import config
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
 
@@ -15,15 +16,16 @@ def feedback(request):
 
     if request.method == 'POST' and form.is_valid():
         feedback_text = form.cleaned_data['text']
-        send_mail(
-            'Вы отправили отзыв',
-            'Вы отправили следующий отзыв:\n'
-            f'{feedback_text}',
-            'from@kittysneakers.example-domain',
-            ['to@kittysneakers.example-domain']
-        )
+
         Feedback.objects.create(
             text=feedback_text
+        )
+        send_mail(
+            'Вы отправили отзыв',
+            f'''Вы отправили следующий отзыв:
+{feedback_text}''',
+            config('SENT_FROM_EMAIL'),
+            [config('ADMIN_EMAIL'), ]
         )
 
         return redirect('feedback:feedback')
