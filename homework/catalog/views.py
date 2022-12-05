bd6b05145bbd9dce3c04aa364bd6fe2d7c4280a9
Alex Godzilla <aadev151@vk.com>
2022-12-05 21:03:28 +0100

    Add rating feature for items

--- homework/catalog/views.py
+++ homework/catalog/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from catalog.models import Item
+from rating.forms import RatingForm
+from rating.models import Rating
 
 
 def item_list(request):
@@ -20,8 +23,36 @@ def item_detail(request, pk):
     item = Item.objects.images(pk)
     if not item:
         return HttpResponseNotFound("it's 404 lol :)")
+
+    initial_data = {
+        'score': Rating.objects.get(user_id=request.user.id).score,
+    }
+
+    rating_form = RatingForm(
+        request.POST or None,
+        initial=initial_data,
+    )
+
+    if request.method == 'POST' and rating_form.is_valid():
+        rating, _ = Rating.objects.get_or_create(
+            item=item,
+            user=request.user,
+        )
+
+        rating.score = rating_form.cleaned_data['score']
+        rating.save()
+
+        return redirect(reverse('users:profile'))
+
+    rating_queryset = Rating.objects.filter(item=item, score__isnull=False)
+    all_ratings = list(map(lambda rating: rating.score, rating_queryset))
+    average = sum(all_ratings) / len(all_ratings)
+
     context = {
         'item': item,
+        'rating_form': rating_form,
+        'average': average,
+        'total': len(all_ratings),
     }
 
     return render(request, template, context=context)
---
+++ homework/rating/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from catalog.models import Item
+from users.models import User
+
+
+class Rating(models.Model):
+    SCORE_CHOICES = [
+        (1, 'Ненависть'),
+        (2, 'Неприязнь'),
+        (3, 'Нейтрально'),
+        (4, 'Обожание'),
+        (5, 'Любовь')
+    ]
+    score = models.PositiveSmallIntegerField(
+        choices=SCORE_CHOICES,
+        verbose_name='Оценка. От 1 до 5, где 1 - "ненависть", 5 - "любовь"',
+        null=True,
+        blank=True,
+    )
+    item = models.ForeignKey(
+        Item,
+        on_delete=models.CASCADE,
+        related_name='rating'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user',
+    )
+
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценки'
+        unique_together = ('user', 'item')
