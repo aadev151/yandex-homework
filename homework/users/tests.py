5dfe7f3002be4acee147c93a399818aaef42a579
Igor-gubkun <Gosha032005@mail.ru>
2022-12-07 21:30:42 +0300

    fix tests

--- homework/users/tests.py
+++ homework/users/tests.py
@@ -26,17 +26,6 @@ class ModelsTest(TestCase):
             "birthday_today",
             response.context,
         )
-
-    def test_birthday_show_correct_context_empty(self):
-        Profile.objects.create_user(
-            first_name="test1", email="test1@gmail.com", password="1234abcd",
-            birthday=date.today() - timedelta(days=5)
-        )
-        Profile.objects.create_user(
-            first_name="test2", email="test2@gmail.com", password="1234abcd",
-            birthday=date.today() - timedelta(days=1)
-        )
-        response = Client().get(reverse('homepage:home'))
         self.assertEqual(len(response.context["birthday_today"]), 0)
 
     def test_birthday_show_correct_context_not_empty(self):
@@ -50,17 +39,5 @@ class ModelsTest(TestCase):
         )
         response = Client().get(reverse('catalog:home'))
         self.assertEqual(len(response.context["birthday_today"]), 1)
-
-    def test_birthday_show_correct_context(self):
-        Profile.objects.create_user(
-            first_name="test1", email="test3@gmail.com", password="1234abcd",
-            birthday=date.today()
-        )
-        Profile.objects.create_user(
-            first_name="test2", email="test4@gmail.com", password="1234abcd",
-            birthday=date.today() - timedelta(days=1)
-        )
-        response = Client().get(reverse('homepage:home'))
-        self.assertEqual(len(response.context["birthday_today"]), 1)
         self.assertEqual(
-            response.context["birthday_today"][0].email, "test3@gmail.com")
+            response.context["birthday_today"][0].email, "test1@gmail.com")
